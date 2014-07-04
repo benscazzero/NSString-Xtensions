4b8974ec4d6d109f92d2a9d8c6e4fa84c591698b
bscazzero42six <bscazzero@42six.com>
2014-07-04 08:25:36 -0400

    Initial Set Up

---
+++ Xtension-Examples/Xtension-Examples/NSString+Xtensions.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Xtensions.h
+//  Xtension-Examples
+//
+//  Created by Ben Scazzero on 7/4/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Xtensions)
+
+@end
---
+++ Xtension-Examples/Xtension-Examples/NSString+Xtensions.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Xtensions.m
+//  Xtension-Examples
+//
+//  Created by Ben Scazzero on 7/4/14.
+//
+//
+
+#import "NSString+Xtensions.h"
+
+@implementation NSString (Xtensions)
+
+@end
